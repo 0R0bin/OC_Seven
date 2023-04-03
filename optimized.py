72f53c7af8a97f4dddc09480c54ee24a0323b1d5
Robin <robinsenechal0@gmail.com>
2023-04-03 21:10:11 +0200

    comment

--- optimized.py
+++ optimized.py
@@ -42,26 +42,3 @@ def knapsack(list_actions, budget_input):
     return K[number_actions][budget]
     
 print(knapsack(actions_and_profits, 500))
-
-
-# def knapSack(W, wt, val, n):
-#  K = [[0 fоr x in rаnge(W + 1)] fоr x in rаnge(n + 1)]
-#  # Build tаble K[][] in bоttоm uр mаnner
-#      for i in range(n + 1):
-#          for w in range(W + 1):
-#              if i == 0  or  w == 0:
-#                  K[i][w] = 0
-#              elif wt[i-1] <= w:
-#                  K[i][w] = max(val[i-1]
-#                            + K[i-1][w-wt[i-1]],
-#                                K[i-1][w])
-#              else:
-#                  K[i][w] = K[i-1][w]
-#      return K[n][W]
-
-#  # Driver code
-#  val = [60, 100, 120]
-#  wt = [10, 20, 30]
-#  W = 50
-#  n = len(val)
-#  print(knapSack(W, wt, val, n))
